e80f849df50b37b5a0ba4b8f9139c8d44a26dc8d
FredrikDalianis <78905580+FredrikDalianis@users.noreply.github.com>
2023-10-25 16:35:08 +0200

    sd

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,54 +1,20 @@
 import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
-
-
-
-
 const TodoItem = ({ todo, handleToggleComplete, handleDelete }) => {
   const { id, text, completed, date } = todo;
 
   return (
     <div className={`todo-item ${completed ? "complete" : ""}`}>
-        <label htmlFor="checkbox-done">{completed ? "Done" : ""}</label>
-        
-        <table colSpan="3">
-       
-       <thead>
-          <tr>
-       
-          </tr>
-       </thead>
-        
-           
+      <label htmlFor="checkbox-done">{completed ? "Done" : ""}</label>
+
+      <table colSpan="3">
+        <thead>
+          <tr></tr>
+        </thead>
+
         <tbody>
           <tr>
-            <td>
-     {/*        {
-              completed ? 
-            
-
-            <FontAwesomeIcon icon={faSquareCheck} beat style={{color: "#97e28d",}} /> 
-            
-            :  
-            <input
-                type="checkbox"
-                id="checkbox-done"
-                checked={completed}
-                onChange={() => handleToggleComplete(id)}
-              />  
-              } */}
-              <input
-                type="checkbox"
-                id="checkbox-done"
-                checked={completed}
-                onChange={() => handleToggleComplete(id)}
-              />  
-            
-
-             
-            </td>
+            <td></td>
             <td>{text}</td>
             <td>{date}</td>
           </tr>
